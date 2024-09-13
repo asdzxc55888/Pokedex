a4c063894b49492afd854d6b1cda17ea4fc7a350
asdzxc55888 <asdzxc55888@gmail.com>
2024-09-13 12:19:38 +0800

    Initial Project

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by 何韋辰 on 2024/9/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
